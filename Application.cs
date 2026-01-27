af58d12bf95b6ed2ee41ea766c8050d47931014a
Abdirahman Dahir <abdirahmanhassandahir5@gmail.com>
2026-01-26 21:35:26 -0500

    Intial commit of Part 2 Lab 1

---
+++ Application.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Xml.Serialization;
+using System.IO;
+/// <summary>
+/// Summary description for Class1
+/// </summary>
+public class Application
+{
+    //Shared instance variable
+    private IList<string> words = new List<string>();
+
+    public IList<string> Words
+    {
+        get { return words; }
+        set { words = value; }
+    }
+
+    public Application()
+    {
+        // optional: initialize or modify words here
+    }
+
+    // 1. The ImportFile method
+    public int ImportFile(String fileName)
+    {
+        
+        try
+        {
+            //read file line by line into an array
+            if (File.Exists(fileName))
+            {
+                using (StreamReader file = new StreamReader(fileName))
+                {
+                    string line;
+                    while ((line = file.ReadLine()) != null)
+                    {
+                        string[] tokens = line.Split(
+                           new[] { ' ', '\t' },
+                           StringSplitOptions.RemoveEmptyEntries
+                        );
+
+                        foreach(String word in tokens)
+                        {
+                            words.Add(word);
+                        }
+                    }
+
+                }
+            }
+            else
+            {
+                Console.WriteLine("File not found.");
+            }
+
+            }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"I/O Error: {ex.Message}");
+        }
+
+        int count = words.Count;
+        return count;
+    }
+
+
+
+    // 2. The BubbleSort method
+    public IList<string> BubbleSort(IList<string> words)
+    {
+        int n = words.Count;
+        String temp;
+        bool swapped;
+
+        for(int i = 0; i < n; i++)
+        {
+            swapped = false;
+            for(int j = 1; j < n - i;j++)
+            {
+                if (String.Compare(words[j], words[j - 1], StringComparison.Ordinal) < 0)
+                {
+                    //swap
+                    temp = words[j - 1];
+                    words[j - 1] = words[j];
+                    words[j] = temp;
+                    swapped = true;
+                }
+
+            }
+            if (!swapped)
+            {
+                break; //list is sorted
+            }
+        }
+
+        return words;
+
+    }
+    // 3. LINQ/Lambda sort words (alphabetically ascending)
+    public IList<string> LINQSort(IList<string> words)
+    {
+        return words.OrderBy(w => w).ToList();
+    }
+
+    //4. Count the distinct words using LINQ
+    public int CountDistinctWords(IList<string> words)
+    {
+        return words.Distinct().Count();
+    }
+
+    //5. Take the first 10 words
+    public  IList<string> First10Words(IList<string> words)
+    {
+        return words.Take(10).ToList();
+    }
+
+    // 6. Reverse each word and print the list.
+    public IList<string> ReverseWords(IList<string> words)
+    {
+        return words.Select(w => new string(w.Reverse().ToArray())).ToList();
+    }
+
+    //7. Get and display the words that end with ‘a’ and display the count
+    public IList<string> WordsEndingWithA(IList<string> words)
+    {
+        var result = words.Where(w => w.EndsWith("a", StringComparison.OrdinalIgnoreCase)).ToList();
+       
+        return result;
+    }
+    // 8. Get and display the words that start with letter ‘m’ and display the count
+    public IList<string> WordsStartingWithM(IList<string> words)
+    {
+        var result = words.Where(w => w.StartsWith("m", StringComparison.OrdinalIgnoreCase)).ToList();
+        return result;
+    }
+
+    //9. Get and display the words that are more than 5 characters long and contain the letter ‘f’, and display the count
+    public IList<string> WordsMoreThan5CharsWithF(IList<string> words)
+    {
+        var result = words.Where(w => w.Length > 5 && w.IndexOf('f', StringComparison.OrdinalIgnoreCase) >= 0).ToList();
+        return result;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,130 @@
+﻿using System.Diagnostics;
+using System.Diagnostics.Tracing;
+
+namespace Lab2
+
+{
+    internal class Program
+    {
+        // The menu method
+        static void Menu()
+        {
+            Console.WriteLine("Select an option: ");
+            Console.WriteLine("1. Import Words from File");
+            Console.WriteLine("2. Bubble Sort words");
+            Console.WriteLine("3. LINQ/Lambda sort words");
+            Console.WriteLine("4. Count Distinct words");
+            Console.WriteLine("5. Display first 10 words");
+            Console.WriteLine("6. Reverse each word and print the list");
+            Console.WriteLine("7. Get and display words that end with 'a' and display the count");
+            Console.WriteLine("8. Get and display words that start with 'm' and display the count");
+            Console.WriteLine("9. Get and display words that are more than 5 characters long and contain the letter 'f', and display the count");
+            Console.WriteLine("x. Exit");
+        }
+        static void Main(string[] args)
+        {
+            Application application = new Application();
+
+            bool fileLoaded = false;
+            int choice = 0;
+        do { 
+            Menu();
+            Console.WriteLine("Enter your choice: ");
+
+            choice = Console.ReadKey().KeyChar;
+            Console.WriteLine(); // move to next line
+            if(application.Words.Count == 0 && choice > 1 && choice != 1)
+                {
+                    Console.WriteLine("Please load words first!.");
+                }
+            
+                switch (choice)
+                {
+                case '1':
+                        if (!fileLoaded)
+                        {
+                         Console.WriteLine("The number of words is: " +
+                         application.ImportFile("C:\\Users\\abdir\\source\\.Net Enterprise\\Lab2\\Words.txt"));
+                        fileLoaded = true;
+
+                        } else
+                        {
+                            Console.WriteLine("File already loaded.");
+                        }
+                        break;
+                case '2':
+                        
+                        Stopwatch stopwatch = Stopwatch.StartNew();
+                        application.BubbleSort(application.Words);
+                        stopwatch.Stop();
+                        TimeSpan ts = stopwatch.Elapsed;
+                        Console.WriteLine($"Execution Time: {ts.TotalMilliseconds} ms");
+
+                        break;
+                case '3':
+                        Stopwatch stopwatchLinq = Stopwatch.StartNew();
+                        application.LINQSort(application.Words);
+                        stopwatchLinq.Stop();
+                        TimeSpan tsLinq = stopwatchLinq.Elapsed;
+                        Console.WriteLine($"Execution Time: {tsLinq.TotalMilliseconds} ms");
+                        break;
+                case '4':
+                        Console.WriteLine("Distinct count is: " + application.CountDistinctWords(application.Words));
+                        break;
+                case '5':
+                       application.First10Words(application.Words);
+                        int n = application.First10Words(application.Words).Count;
+                        for (int i = 0; i < n; i++)
+                        {
+                            Console.WriteLine(application.First10Words(application.Words)[i] + '\t');
+                        }
+
+                        break;
+                case '6':
+                        application.ReverseWords(application.Words);
+                        int m = application.ReverseWords(application.Words).Count;
+                        for (int i = 0; i < m; i++)
+                        {
+                            Console.WriteLine(application.ReverseWords(application.Words)[i] + '\t');
+                        }
+                        break;
+                case '7':
+                        application.WordsEndingWithA(application.Words);
+                        int p = application.WordsEndingWithA(application.Words).Count;
+                        Console.WriteLine($"The {p} words that end with 'a' are:");
+
+                        for (int i = 0; i < p; i++)
+                        {
+                            Console.WriteLine(application.WordsEndingWithA(application.Words)[i] + '\t');
+                        }
+                        break;
+                case '8': 
+                        application.WordsStartingWithM(application.Words);
+                        int q = application.WordsStartingWithM(application.Words).Count;
+                        Console.WriteLine($"The {q} words that start with 'm' are:");
+                        for (int i = 0; i < q; i++)
+                        {
+                            Console.WriteLine(application.WordsStartingWithM(application.Words)[i] + '\t');
+                        }
+                        break;
+                case '9':
+                        application.WordsMoreThan5CharsWithF(application.Words);
+                        int r = application.WordsMoreThan5CharsWithF(application.Words).Count;
+                        Console.WriteLine($"The {r} words that are more than 5 characters long and contain the letter 'f' are:");
+                        for (int i = 0; i < r; i++)
+                        {
+                            Console.WriteLine(application.WordsMoreThan5CharsWithF(application.Words)[i] + '\t');
+                        }
+
+                        break;
+                    case 'x':
+                        Console.WriteLine("Exiting the program.");
+                        break;
+                default: Console.WriteLine("..Invalid input please, try again...");
+                        break;
+                }
+
+            }while (choice != 'x');
+        }
+    }
+}
